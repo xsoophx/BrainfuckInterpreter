feb919b7134008fe42efb20de44d1391091110e1
Sophia Köhler <sophia.koehler@staffbase.com>
2020-01-29 22:35:05 +0100

    :tada: Initial commit

--- src/main/MainInterpreter.kt
+++ src/main/MainInterpreter.kt
@@ -1,14 +1,77 @@
 package main
 
+
 fun main(){
     val currentBaseClass = MainInterpreterBase()
-
-    currentBaseClass.`print final interpreted output`()
+    currentBaseClass.printfinalOutput()
 }
+
+
 
 class MainInterpreterBase{
 
-    fun `print final interpreted output`(){
-        println("initial commit")
+    init {
+        var BracketStack = setOf<Char>()
     }
+
+    companion object{
+        @JvmStatic
+        val outputOfCurrentText = "Here will be test Objects for interpreting"
+
+        val inputCommand = "[ This program prints \"Hello World!\" and a newline to the screen, its\n" +
+                "  length is 106 active command characters. [It is not the shortest.]\n" +
+                "\n" +
+                "  This loop is an \"initial comment loop\", a simple way of adding a comment\n" +
+                "  to a BF program such that you don't have to worry about any command\n" +
+                "  characters. Any \".\", \",\", \"+\", \"-\", \"<\" and \">\" characters are simply\n" +
+                "  ignored, the \"[\" and \"]\" characters just have to be balanced. This\n" +
+                "  loop and the commands it contains are ignored because the current cell\n" +
+                "  defaults to a value of 0; the 0 value causes this loop to be skipped.\n" +
+                "]\n" +
+                "++++++++               Set Cell #0 to 8\n" +
+                "["
+    }
+
+    private enum class ValidCharacters (val PairOfCommands: Pair<Char, Char>){
+        PointerIncrementDecrement(Pair('>', '<')),
+        PointerValueIncrementDecrement (Pair('+', '-')),
+        PutGetChar (Pair('.', ',')),
+        WhileLoop(Pair('[', ']')),
+    }
+
+    fun printfinalOutput(){
+        println(outputOfCurrentText)
+    }
+
+    fun enumClassContains(currentCharacter: Char): Boolean {
+        return enumValues<ValidCharacters>().any {
+            it.PairOfCommands.first == currentCharacter || it.PairOfCommands.second == currentCharacter
+        }
+    }
+
+    fun cleanedUpVariable (input: String): String {
+        var cleanedUpCommand = ""
+        input.forEach { char ->
+            if (enumClassContains(char))
+                cleanedUpCommand += char
+        }
+        return cleanedUpCommand
+    }
+
+    fun getComplementaryPairValue (givenValue: Char): Char {
+            ValidCharacters.values().find {
+                  (it.PairOfCommands.first == givenValue)
+                    return complementOfPair(it.PairOfCommands, givenValue)
+            }
+            ValidCharacters.values().find {
+                (it.PairOfCommands.second == givenValue)
+                return complementOfPair(it.PairOfCommands, givenValue)
+            }
+
+    }
+
+    fun complementOfPair(currentPair: Pair<Char, Char>, givenChar: Char): Char{
+        return if (currentPair.first == givenChar) currentPair.second else currentPair.first
+    }
+
 }
--- src/test/MamaTest.kt
+++ src/test/MamaTest.kt
@@ -1,34 +1,104 @@
 import main.MainInterpreterBase
 import org.junit.Assert.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import kotlin.test.assertNull
 import java.util.stream.Stream
 
 internal class MamaTest {
+    private lateinit var newBaseClass: MainInterpreterBase
 
-    companion object{
+    @BeforeEach
+    fun SetUp(){
+        newBaseClass = MainInterpreterBase()
+    }
+
+    companion object {
         @JvmStatic
         fun `Data for testing interpreter`() = Stream.of(
             Arguments.of(
-                "Here will be test Objects for interpreting"),
+                "Here will be test Objects for interpreting",
+                MainInterpreterBase.outputOfCurrentText
+                )
+        )
+
+        @JvmStatic
+        fun `Strings with Brackets` () = Stream.of(
             Arguments.of(
-                "And here will be another String for testing"
+                    "[This is another Test]", "[]"
+            ),
+            Arguments.of(
+                "++++++++++\n" +
+                        " [\n" +
+                        "  >+++++++>++++++++++>+++>+<<<<-\n" +
+                        " ]                       Schleife zur Vorbereitung der Textausgabe\n" +
+                        " >++.                    Ausgabe von 'H'\n" +
+                        " >+.                     Ausgabe von 'e'\n" +
+                        " +++++++.                'l'\n" +
+                        " .                       'l'\n" +
+                        " +++.                    'o'\n" +
+                        " >++.                    Leerzeichen\n" +
+                        " <<+++++++++++++++.      'W'\n" +
+                        " >.                      'o'\n" +
+                        " +++.                    'r'\n" +
+                        " ------.                 'l'\n" +
+                        " --------.               'd'\n" +
+                        " >+.                     '!'\n" +
+                        " >.                      Zeilenvorschub\n" +
+                        " +++.                    Wagenrücklauf",
+                "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++." +
+                        "------.--------.>+.>.+++."
             )
+        )
+        @JvmStatic
+        fun `Matching opening and closing chars`()= Stream.of(
+            Arguments.of(
+                "+++++ +++               Set Cell #0 to 8\n" +
+                        " 2 [" , 9
+            ),
+            Arguments.of(
+                "[<]", 3
+
+        )
+        )
+        @JvmStatic
+        fun `Valid and invalid Chars`()= Stream.of(
+            Arguments.of(
+                '+' , true),
+            Arguments.of(
+                '>', true),
+            Arguments.of(
+                'f', false
+        )
         )
         }
 
-    @Test
-    fun `initialize Base class`() {
-        val newBaseClass = MainInterpreterBase()
-        assertNull(newBaseClass)
+    @ParameterizedTest
+    @MethodSource("Matching opening and closing chars")
+    fun `Count opening and closing symbols`(actualInput: String, expected: Int){
+        var numberOfCharsInInput = newBaseClass.cleanedUpVariable(actualInput)
+        assertEquals(expected, numberOfCharsInInput.count())
     }
 
     @ParameterizedTest
+    @MethodSource("Valid and invalid Chars")
+    fun `Does enum class contain character`(actualInput: Char, expected: Boolean){
+        var enumContainsChar = newBaseClass.enumClassContains(actualInput)
+        assertEquals(expected, enumContainsChar)
+    }
+
+    @Disabled
     @MethodSource("Data for testing interpreter")
-    fun`Test correct output after interpreting`(actualOutput: String){
-        assertEquals("", "actualOutput")
+    fun`Test correct output after interpreting`(expected: String, actualOutput: String){
+       assertEquals( "Here will be test Objects for interpreting", actualOutput)
+    }
+
+    @ParameterizedTest
+    @MethodSource("Strings with Brackets")
+    fun `Everything between actual BF chars should be removed`(actualInput: String, expected: String){
+        var cleanedUpString = newBaseClass.cleanedUpVariable(actualInput)
+        assertEquals(expected, cleanedUpString)
     }
 }
